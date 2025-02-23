ccbd7f1d837d148914ef32c592abe7c4c8c38171
acs374 <acs374@cornell.edu>
2025-02-23 18:23:47 -0500

    lots of progress done

--- logic.c
+++ logic.c
@@ -18,8 +18,19 @@ info decode(uint32_t instruction)
     uint32_t opcode = instruction & 0x7F;         
     uint32_t rd = (instruction >> 7) & 0x1F;  
     uint32_t funct3 = (instruction >> 12) & 0x7; 
+    uint32_t rs2   = (instruction >> 20) & 0x1F;  
+    uint32_t funct7 = (instruction >> 25) & 0x7F; 
 
-    // 
+    // sign extension for immediate
+    int32_t extended;
+    if (imm12 & 0x800) {
+        extended = (int32_t)(imm12 | 0xFFFFF000);
+    } else {
+        extended = (int32_t)imm12;
+    }
+
+
+    // TYPE I
     if (opcode == 0x13) {
         switch (funct3) {
             // ADDI
@@ -45,22 +56,108 @@ info decode(uint32_t instruction)
         }
 
         decoded.first = registers[rs1];
-        decoded.second = 
+        decoded.second = extended;
         decoded.third = rd;
-    } else {
-        decoded.inst = 0;
-        decoded.first = 0;
-        decoded.second = 0;
-        decoded.third = 0;
+    // alternate type I opcode
+    } else if (opcode == 0x03) {
+        switch (funct3) {
+            // LB
+            case 0x0: 
+                decoded.inst = LB;
+                break;
+            // LW
+            case 0x2: 
+                decoded.inst = LW;
+                break;
+            // LD
+            case 0x3:
+                decoded.inst = LD;
+                break;
+            default:
+                decoded.inst = 0;
+                break;
+        }
+        decoded.first = registers[rs1];    
+        decoded.second = extended;         
+        decoded.third = rd;  
+
+    // TYPE R    
+    } else if (opcode == 0x33) {
+        // 
+        uint32_t rd_r  = (instruction >> 7) & 0x1F;
+        switch (funct3) {
+            case 0x0:
+                if (funct7 == 0x20) {
+                    // SUB
+                    decoded.inst = SUB;
+                } else {
+                    // ADD
+                    decoded.inst = ADD;
+                }
+                break;
+            case 0x7:
+                // AND
+                decoded.inst = AND;
+                break;
+            case 0x2:
+                // SLT
+                decoded.inst = SLT;
+                break;
+            case 0x1:
+                // SLL
+                decoded.inst = SLL;
+                break;
+            case 0x5:
+                if (funct7 == 0x20) {
+                    // SRA
+                    decoded.inst = SRA;
+                }
+                break;
+            default:
+                decoded.inst = 0;
+                break;
+        }
+        decoded.first  = registers[rs1];
+        decoded.second = registers[rs2];
+        decoded.third  = rd_r;
+    }
+    // TYPE U
+    else if (opcode == 0x37) {
+        decoded.inst = LUI;
+        uint64_t upper = (uint64_t)(instruction & 0xFFFFF000);
+        decoded.first = 0;       
+        decoded.second = upper;  
+        decoded.third = rd;      
+    }
+    // TYPE B
+    else if (opcode == 0x63) {
+        uint32_t rs2_b = (instruction >> 20) & 0x1F;
+        int32_t branch_imm = 0;
+        branch_imm |= ((instruction >> 31) & 0x1) << 12;
+        branch_imm |= ((instruction >> 25) & 0x3F) << 5;
+        branch_imm |= ((instruction >> 8) & 0xF) << 1;
+        branch_imm |= ((instruction >> 7) & 0x1) << 11;
+        if (branch_imm & 0x1000) {
+            branch_imm |= 0xFFFFE000;
+        }
+        switch (funct3) {
+            case 0x0: 
+                decoded.inst = BEQ;
+                break;
+            default:
+                decoded.inst = 0;
+                break;
+        }
+        decoded.first  = registers[rs1];   
+        decoded.second = registers[rs2_b]; 
+        decoded.third  = (uint64_t)branch_imm;
     }
 
-    // sign extension for immediate
-    int32_t extended;
-    if (imm12 & 0x800) {
-        extended = (int32_t)(imm12 | 0xFFFFF000);
-    } else {
-        extended = (int32_t)imm12;
-    }
+    
+
+
+
+
 
     return decoded;
     
@@ -69,14 +166,40 @@ info decode(uint32_t instruction)
 // TODO
 info execute(info information)
 {
+    switch (information.inst) {
+        // ADDI
+        case ADDI:
+            information.first = information.first + information.second;
+            break;
+        // ORI
+        case ORI:
+            information.first = information.first | information.second;
+            break;
+        // ANDI
+        case ANDI:
+            information.first = information.first & information.second;
+            break;
+        // XORI
+        case XORI:
+            information.first = information.first ^ information.second;
+            break;
+        default:
+            break;
+    }
+    return information;
 }
 
 // TODO
 info memory(info information)
 {
+    return information;
 }
 
 // TODO
 uint64_t writeback(uint64_t PC, info information)
 {
+    if (information.inst != 0) {
+        registers[information.third] = information.first;
+    }
+    return PC + 4;
 }
