2f3deed376387fc898d3eeb80b936e1d880d19a1
acs374 <acs374@cornell.edu>
2025-02-24 18:39:35 -0500

    testing work progress

--- check.s
+++ check.s
@@ -1,15 +1,15 @@
 # TYPE I - General Cases for ADDI
 #addi ra,zero,0x155
 #addi t0,zero,0x13ddf4
-#
+
 #  TYPE I - Edge Cases for ADDI
 #addi t1,zero,-2048
 #addi t2,zero,2047
 #addi t3,t1,1
-#
+
 # TYPE I - General Cases for ANDI
 #andi sp,ra,0x1b9
-#
+
 # TYPE I - Edge Cases for ANDI
 #addi t1,zero,0xfff
 #andi t1,t1,-2048
@@ -17,21 +17,21 @@
 #andi t2,t2,2047
 #addi t3,zero,0xabcd
 #andi t3,t3,-1
-#
+
 # TYPE R - General Cases
-addi t0,zero,10
-addi t1,zero,4
-add t2,t0,t1
-sub t3,t0,t1
-and t4,t0,t1
-sll t5,t0,t1
-sra t6,t5,t1
+#addi t0,zero,10
+#addi t1,zero,4
+#add t2,t0,t1
+#sub t3,t0,t1
+#and t4,t0,t1
+#sll t5,t0,t1
+#sra t6,t5,t1
 
 # TYPE R - Edge Cases
-#addi t7,zero,-1
-#add t8,t7,1
-#sub t9,zero,t7
-#
+addi t3,zero,-1   # expected 0xffffffffffffffff at register t3 (28)
+add t4,t3,1       # expected 0x0 at register t4 (29)
+sub t5,zero,t3    # e
+
 # TYPE U
 #lui ra,zero,0x65432
 #
